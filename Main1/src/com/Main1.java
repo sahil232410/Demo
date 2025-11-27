950ba939e0ff4ca849f029d3e57340d1a9a6b525
Admin <Admin@DESKTOP-LMJPBRH>
2025-11-27 09:16:48 +0530

    maths operation

---
+++ Main1/src/com/Main1.java
@@ -0,0 +1,35 @@
+package com;
+
+public interface Main1 {
+	void add();
+	void sub();
+	void mul();
+	void div();
+
+}
+class A implements Main1{
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sub() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mul() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void div() {
+		// TODO Auto-generated method stub
+		
+	}
+}
